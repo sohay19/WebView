14227c19c7d59cc52669f87480cd03ca7a43e672
soha <sa4022@naver.com>
2023-02-02 18:43:27 +0900

    Initial Commit

---
+++ WebView/WebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewApp.swift
+//  WebView
+//
+//  Created by 소하 on 2023/02/02.
+//
+
+import SwiftUI
+
+@main
+struct WebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
